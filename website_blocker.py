f7becdb921086aefe126c3cb4cd1b7a028a7968c
gregorytcheng <gregorytcheng@gmail.com>
2019-03-09 21:51:37 -0500

    First commit

---
+++ website_blocker.py
@@ -0,0 +1,13 @@
+import time
+from datetime import datetime as dt
+
+host_path = "/etc/hosts"
+redirect_ip_address = "127.0.0.1"
+website_list = ["www.facebook.com", "facebook.com"]
+
+
+while True:
+    # if dt.now():
+
+    print(1)
+    time.sleep(10)
